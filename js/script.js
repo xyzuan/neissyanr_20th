4c75427cae6ff6e0d2c3dba11e7eee94502ac492
xyzuan <xyzuannihboss@gmail.com>
2021-11-08 15:39:42 +0700

    sya: Restyle-ing font

--- js/script.js
+++ js/script.js
@@ -222,7 +222,7 @@ const animationTimeline = () => {
       {
         scale: 1,
         rotationY: 0,
-        color: "#ff69b4",
+        color: "#434141;",
         ease: Expo.easeOut,
       },
       0.1,
